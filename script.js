0b7edfa48d161baca79bd8d20522c1ba59d2dd75
Tine <111304307+TineMSPetersen@users.noreply.github.com>
2023-09-23 16:22:53 +0200

    Make input display result
    Added bugs.json to hold data while coding to prevent repeated unnecessary API calls

--- script.js
+++ script.js
@@ -1,51 +1,64 @@
+// Creates and appends the results (bugApiData) to the parent element
+  function CreateSearchResult(bugApiData, parentElement) {
+    const resultBox = document.createElement("div");
+    resultBox.classList.add("result-box");
+      
+    // Create the Common Name Heading
+    const commonName = document.createElement("h2");
+    commonName.textContent = bugApiData.CommonName;
 
-fetch("https://bugs.verfasor.com/api")
-  .then(function (response) {
-    if (!response.ok) {
-      throw new Error("Failed to fetch the Incest API");
-    }
-    console.log("Fetched correctly");
-    return response.json();
-  })
-  .then(function (bugs) {
-    const element = document.querySelector("#query-result");
+    // Create the Scientific Name Heading
+    const scientificName = document.createElement("h3");
+    scientificName.textContent = `${bugApiData.Genus} ${bugApiData.Species}`
+
+    // Create the link
+    const bugGuideURL = document.createElement("a");
+    bugGuideURL.textContent = "More Info";
+    bugGuideURL.href = bugApiData.URL;
+
+    // Appends all the elements to the resultbox
+    resultBox.appendChild(commonName);
+    resultBox.appendChild(scientificName);
+    resultBox.appendChild(bugGuideURL);
+
+    // Apends the resultbox (Filled with content now) to the parentElement
+    parentElement.appendChild(resultBox);
+  }
+
+  document
+  .querySelector("#search-form")
+  .addEventListener("submit", function (event) {
+    // Prevents the form from submitting and reloading the page
+    event.preventDefault();
+    
+    // Gets the string from the user input
+    const searchQuery = document.querySelector("#search-query").value;
+    console.log(searchQuery);
+
+    // Fetches the bug API
+    fetch("bugs.json")
+    .then(function (response) {
+      if (!response.ok) {
+      throw new Error("Failed to fetch the Insect API");
+      }
+      console.log("Fetched correctly");
+      return response.json();
+    })
+    .then(function (bugs) {
+    
     let loadedResults = 0;
     for (const bug of bugs) {
-      if (loadedResults >= 10) {
-        break; }
-      CreateSearchResult(bug, element);
-      loadedResults++;
+      MatchQueryToResult(searchQuery, bug)
+    }
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+
+    function MatchQueryToResult(query, bugApiData) {
+      if (query == "Brown-hooded Cockroach" && bugApiData.CommonName == "Brown-hooded Cockroach") {
+        const element = document.querySelector("#query-result");
+        CreateSearchResult(bugApiData, element);
+      }
     }
   })
-  .catch(function (error) {
-    console.error(error);
-  });
-
-
-  function CreateSearchResult(bugApiData, parentElement) {
-    if (parentElement) {
-      const resultBox = document.createElement("div");
-      resultBox.classList.add("result-box");
-      
-      // Create the Common Name Heading
-      const commonName = document.createElement("h2");
-      commonName.textContent = bugApiData.CommonName;
-
-      // Create the Scientific Name Heading
-      const scientificName = document.createElement("h3");
-      scientificName.textContent = `${bugApiData.Genus} ${bugApiData.Species}`
-
-      // Create the link
-      const bugGuideURL = document.createElement("a");
-      bugGuideURL.textContent = "More Info";
-      bugGuideURL.href = bugApiData.URL;
-
-      resultBox.appendChild(commonName);
-      resultBox.appendChild(scientificName);
-      resultBox.appendChild(bugGuideURL);
-
-      // Apends everything to the parentElement
-      parentElement.appendChild(resultBox);
-    }
-  }
-  CreateSearchResult(element);
