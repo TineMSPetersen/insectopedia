1a2fa3f307d4d712d87ac07f54680f0d3715ca73
Tine <111304307+TineMSPetersen@users.noreply.github.com>
2023-09-23 15:56:03 +0200

    Create element for API results

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Fetch bug API
+
 fetch("https://bugs.verfasor.com/api")
   .then(function (response) {
     if (!response.ok) {
@@ -8,10 +8,44 @@ fetch("https://bugs.verfasor.com/api")
     return response.json();
   })
   .then(function (bugs) {
+    const element = document.querySelector("#query-result");
+    let loadedResults = 0;
     for (const bug of bugs) {
-      AppendBug(bug);
+      if (loadedResults >= 10) {
+        break; }
+      CreateSearchResult(bug, element);
+      loadedResults++;
     }
   })
   .catch(function (error) {
     console.error(error);
   });
+
+
+  function CreateSearchResult(bugApiData, parentElement) {
+    if (parentElement) {
+      const resultBox = document.createElement("div");
+      resultBox.classList.add("result-box");
+      
+      // Create the Common Name Heading
+      const commonName = document.createElement("h2");
+      commonName.textContent = bugApiData.CommonName;
+
+      // Create the Scientific Name Heading
+      const scientificName = document.createElement("h3");
+      scientificName.textContent = `${bugApiData.Genus} ${bugApiData.Species}`
+
+      // Create the link
+      const bugGuideURL = document.createElement("a");
+      bugGuideURL.textContent = "More Info";
+      bugGuideURL.href = bugApiData.URL;
+
+      resultBox.appendChild(commonName);
+      resultBox.appendChild(scientificName);
+      resultBox.appendChild(bugGuideURL);
+
+      // Apends everything to the parentElement
+      parentElement.appendChild(resultBox);
+    }
+  }
+  CreateSearchResult(element);
